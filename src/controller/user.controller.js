5b4c043f36b062d6127b5b7049853cd9a6fa8ae7
samarthio10 <samarth.ksharma04@gmail.com>
2026-08-14 22:53:07 +0530

    authorization

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-import asyncHandler from '../utils/ayncHandler.js';
+import asyncHandler from '../utils/asyncHandler.js';
 import ApiError from '../utils/apiError.js';
 import { User } from '../models/user.model.js';
 import uploadOnCloudinary from '../utils/cloudinary.js';
